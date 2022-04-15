3f65c0a1bc1f9359c5bf673ef076c2591423b1f9
weiss <kingbaiing@163.com>
2022-04-15 13:58:57 +0200

    update json keys name and order of players.

--- client/src/routes/doudizhuInput.js
+++ client/src/routes/doudizhuInput.js
@@ -1,6 +1,6 @@
 import '../App.css'
 import React from 'react'
-import {post} from '../common/api'
+import { post } from '../common/api'
 import { PlayerList, INum, UserList } from '../common/input'
 import { InputNumber, Button } from 'rsuite'
 import Header from '../navbar/navbar'
@@ -8,7 +8,7 @@ import { playerValue, playersArray } from '../common/players'
 
 const defaultOption = {
     players: ['unknown', 'unknown', 'unknown', 'unknown'],
-    points: 8,
+    rawPoints: 8,
     winner: '',
     weight: {},
     lord: '',
@@ -19,7 +19,7 @@ const defaultOption = {
 
 const DouDiZhuInput = ({ option }) => {
     const [players, setPlayers] = React.useState(option.players)
-    const [points, setPoints] = React.useState(option.points)
+    const [rawPoints, setRawPoints] = React.useState(option.rawPoints)
     const [winner, setWinner] = React.useState(option.winner)
     const [weight, setWeight] = React.useState(option.weight)
     const [lord, setLord] = React.useState(option.lord)
@@ -29,7 +29,7 @@ const DouDiZhuInput = ({ option }) => {
     function handleSubmit(event) {
         const res = {
             players: players,
-            points: points,
+            rawPoints: rawPoints,
             winner: winner,
             weight: weight,
             lord: lord,
@@ -53,11 +53,11 @@ const DouDiZhuInput = ({ option }) => {
                     <div />
                     <div> <PlayerList key='user0' player={players[0]} playerOnChange={key => updatePlayer(key, 0)} /> </div>
                     <div />
+                    <div> <PlayerList key='user3' player={players[3]} playerOnChange={key => updatePlayer(key, 3)} /> </div>
+                    <div />
                     <div> <PlayerList key='user1' player={players[1]} playerOnChange={key => updatePlayer(key, 1)} /> </div>
                     <div />
                     <div> <PlayerList key='user2' player={players[2]} playerOnChange={key => updatePlayer(key, 2)} /> </div>
-                    <div />
-                    <div> <PlayerList key='user3' player={players[3]} playerOnChange={key => updatePlayer(key, 3)} /> </div>
                     <div />
                 </div>
                 <div class='mt-2 pb-4 max-w-sm'>
@@ -87,7 +87,7 @@ const DouDiZhuInput = ({ option }) => {
                     ))}
 
                     <br />
-                    <InputNumber prefix='Points' value={points} onChange={(n, _) => setPoints(n)} min={8} />
+                    <InputNumber prefix='RawPoints' value={rawPoints} onChange={(n, _) => setRawPoints(n)} min={8} />
                     <br />
                 </div>
                 <Button appearance='primary' onClick={handleSubmit}>Submit</Button>
