0a970d309a9c4aeb156bd9bf1db4edff2adceeab
weiss <kingbaiing@163.com>
2022-05-13 22:04:48 +0200

    make router support refresh

--- router/handler/spaHandler.go
+++ router/handler/spaHandler.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"errors"
 	"io/fs"
 	"net/http"
+	"strings"
 )
 
 type spaHandler struct {
@@ -14,7 +16,23 @@ func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	http.FileServer(http.FS(fsys)).ServeHTTP(w, r)
+
+	path := r.URL.Path
+	_, err = fs.Stat(fsys, strings.Trim(path, "/"))
+	switch {
+	case err == nil, path == "/", strings.HasPrefix(path, "/api/"):
+		http.FileServer(http.FS(fsys)).ServeHTTP(w, r)
+	case errors.Is(err, fs.ErrNotExist):
+		// use react router, redirect to index 
+		content, err := fs.ReadFile(fsys, "index.html")
+		if err != nil {
+			panic(err)
+		}
+		w.Write(content)
+	default:
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	return
 }
 
 func New(fs fs.FS) spaHandler {
